c3610c52d134b9431ef0ecf07d36a3a8274865d5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2026-03-15 23:12:36 +0530

    Add files via upload

---
+++ Print_binary_tree_in_vertical_order.java
@@ -0,0 +1,42 @@
+class Pair {
+    Node node;
+    int hd;
+
+    Pair(Node node, int hd) {
+        this.node = node;
+        this.hd = hd;
+    }
+}
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> verticalOrder(Node root) {
+        
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+
+        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(root, 0));
+
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            Node curr = p.node;
+            int hd = p.hd;
+
+            map.putIfAbsent(hd, new ArrayList<>());
+            map.get(hd).add(curr.data);
+
+            if(curr.left != null)
+                q.add(new Pair(curr.left, hd - 1));
+
+            if(curr.right != null)
+                q.add(new Pair(curr.right, hd + 1));
+        }
+
+        for(ArrayList<Integer> list : map.values())
+            res.add(list);
+
+        return res;
+    }
+}
